e04d7c2e6f6f7e5487433f2c0564182ff260e340
shivani0614 <shivanialeti14@gmail.com>
2024-02-01 15:48:55 -0700

    reverted to original form

--- server.js
+++ server.js
@@ -78,71 +78,57 @@
 // // const PORT =  3000;
 // // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-// const express = require("express");
-// const router = express.Router();
-// const cors = require("cors");
-// const nodemailer = require("nodemailer");
-
-// // server used to send send emails
-// const app = express();
-// app.use(cors());
-// app.use(express.static('public'));
-// app.use(express.json());
-// app.use("/", router);
-// app.listen(3000, () => console.log("Server Running"));
-// console.log(process.env.EMAIL_USER);
-// console.log(process.env.EMAIL_PASS);
-
-// const contactEmail = nodemailer.createTransport({
-//   service: 'gmail',
-//   auth: {
-//     user: "shivaniproff@gmail.com",
-//     pass: "nmpg kpqn icei doay"
-//   },
-// });
-
-// contactEmail.verify((error) => {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log("Ready to Send");
-//   }
-// });
-
-// router.post("/contact", (req, res) => {
-//   const name = req.body.firstName + req.body.lastName;
-//   const email = req.body.email;
-//   const message = req.body.message;
-//   const phone = req.body.phone;
-//   const mail = {
-//     from: name,
-//     // to: "shivaniproff@gmail.com",
-//     subject: "Contact Form Submission - Portfolio",
-//     html: `<p>Name: ${name}</p>
-//            <p>Email: ${email}</p>
-//            <p>Phone: ${phone}</p>
-//            <p>Message: ${message}</p>`,
-//   };
-//   contactEmail.sendMail(mail, (error) => {
-//     if (error) {
-//       res.json(error);
-//     } else {
-//       res.json({ code: 200, status: "Message Sent" });
-//     }
-//   });
-// });
-
 const express = require("express");
-const app = express();
+const router = express.Router();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+
 // server used to send send emails
-const PORT = process.env.PORT || 3000;
-
+const app = express();
+app.use(cors());
 app.use(express.static('public'));
+app.use(express.json());
+app.use("/", router);
+app.listen(3000, () => console.log("Server Running"));
+console.log(process.env.EMAIL_USER);
+console.log(process.env.EMAIL_PASS);
 
-app.get('/',(req,res)=>{
-  // res.send('Hello!')
-  res.sendFile(__dirname + '/public/index.html')
-})
-app.listen(PORT, () => {
-  console.log(`Server Running on port ${PORT}`)
+const contactEmail = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: "shivaniproff@gmail.com",
+    pass: "nmpg kpqn icei doay"
+  },
 });
+
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
+});
+
+router.post("/contact", (req, res) => {
+  const name = req.body.firstName + req.body.lastName;
+  const email = req.body.email;
+  const message = req.body.message;
+  const phone = req.body.phone;
+  const mail = {
+    from: name,
+    // to: "shivaniproff@gmail.com",
+    subject: "Contact Form Submission - Portfolio",
+    html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Phone: ${phone}</p>
+           <p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json({ code: 200, status: "Message Sent" });
+    }
+  });
+});
+

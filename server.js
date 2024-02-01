f12caaf88f1eee7050e3b6dafe20f7fd752ea823
shivani0614 <shivanialeti14@gmail.com>
2024-02-01 15:26:17 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,148 @@
+// require('dotenv').config();
+// const express = require("express");
+// const router = express.Router();
+// const cors = require("cors");
+// const nodemailer = require("nodemailer");
+
+// // server used to send send emails
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+// app.use("/", router);
+// app.get("*", (req, res) => {
+//   res.send("Server is running");
+// });
+// app.listen(3000, () => console.log("Server Running"));
+// const EMAIL_USER = process.env.EMAIL_USER;
+// const EMAIL_PASS = process.env.EMAIL_PASS;
+
+// console.log(process.env.EMAIL_USER);
+// console.log(process.env.EMAIL_PASS);
+
+// // const contactEmail = nodemailer.createTransport({
+// //   service: 'gmail',
+// //   auth: {
+// //     user: "********@gmail.com",
+// //     pass: ""
+// //   },
+// // });
+// const contactEmail = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: EMAIL_USER,
+//     pass: EMAIL_PASS
+//   }
+// });
+
+
+// contactEmail.verify((error) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("Ready to Send");
+//   }
+// });
+
+// router.post("/contact", (req, res) => {
+//   const name = req.body.firstName + req.body.lastName;
+//   const email = req.body.email;
+//   const message = req.body.message;
+//   const phone = req.body.phone;
+//   const mail = {
+//     from: name,
+//     // to: "********@gmail.com",
+//     to: "shivaniproff@gmail.com",
+//     subject: "Contact Form Submission - Portfolio",
+//     html: `<p>Name: ${name}</p>
+//            <p>Email: ${email}</p>
+//            <p>Phone: ${phone}</p>
+//            <p>Message: ${message}</p>`,
+//   };
+//   // contactEmail.sendMail(mail, (error) => {
+//   //   if (error) {
+//   //     res.json(error);
+//   //   } else {
+//   //     res.json({ code: 200, status: "Message Sent" });
+//   //   }
+//   // });
+//   contactEmail.sendMail(mail, (error) => {
+//     if (error) {
+//       res.status(500).json({ error: 'Internal Server Error' });
+//     } else {
+//       res.status(200).json({ message: 'Message Sent Successfully' });
+//     }
+//   });
+
+// });
+
+// // const PORT =  3000;
+// // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// const express = require("express");
+// const router = express.Router();
+// const cors = require("cors");
+// const nodemailer = require("nodemailer");
+
+// // server used to send send emails
+// const app = express();
+// app.use(cors());
+// app.use(express.static('public'));
+// app.use(express.json());
+// app.use("/", router);
+// app.listen(3000, () => console.log("Server Running"));
+// console.log(process.env.EMAIL_USER);
+// console.log(process.env.EMAIL_PASS);
+
+// const contactEmail = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: "shivaniproff@gmail.com",
+//     pass: "nmpg kpqn icei doay"
+//   },
+// });
+
+// contactEmail.verify((error) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("Ready to Send");
+//   }
+// });
+
+// router.post("/contact", (req, res) => {
+//   const name = req.body.firstName + req.body.lastName;
+//   const email = req.body.email;
+//   const message = req.body.message;
+//   const phone = req.body.phone;
+//   const mail = {
+//     from: name,
+//     // to: "shivaniproff@gmail.com",
+//     subject: "Contact Form Submission - Portfolio",
+//     html: `<p>Name: ${name}</p>
+//            <p>Email: ${email}</p>
+//            <p>Phone: ${phone}</p>
+//            <p>Message: ${message}</p>`,
+//   };
+//   contactEmail.sendMail(mail, (error) => {
+//     if (error) {
+//       res.json(error);
+//     } else {
+//       res.json({ code: 200, status: "Message Sent" });
+//     }
+//   });
+// });
+
+const express = require("express");
+const app = express();
+// server used to send send emails
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+  // res.send('Hello!')
+  res.sendFile(__dirname + '/public/index.html')
+})
+app.listen(PORT, () => {
+  console.log(`Server Running on port ${PORT}`)
+});
---
+++ src/components/Projects.js
@@ -0,0 +1,99 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import covidMap from "../assets/img/covid-map.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "•	Movie Reviews Application: ",
+      description: "Developed an engaging Movie Reviews web application utilizing AngularJS, offering users an interactive platform to browse, review, and rate movies. Utilized: JSON, Bootstrap, Angular CLI, Rest API’s, dynamic pages, Routing.",
+      imgUrl: projImg1,
+    },
+    {
+      title: "•	Weather Forecast App: ",
+      description: "Designed and developed a Weather Forecast App using React Native, demonstrating proficiency in mobile application development. The app provides weather updates, forecasts, and interactive features for users. Integrated APIs to fetch and display accurate weather data. Utilized: React Native, JavaScript, APIs for weather data retrieval, Responsive Design.",
+      imgUrl: covidMap,
+    },
+    {
+      title: "•	Covid Map Application: ",
+      description: "Developed a COVID Map Application using React.js to visualize real-time data and statistics related to the COVID pandemic. The application provides an interactive map interface for users to explore and understand the impact of the virus globally or within specific regions.",
+      imgUrl: projImg1,
+    },
+    {
+      title: "•	Web Data Management Project:",
+      description: "Implemented client-server architecture with RPC protocols, enabling seamless file actions such as uploading, deleting, and renaming, optimizing synchronous and asynchronous operations. Leveraged JAVA, VMware, and Ubuntu technologies.",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="section">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}

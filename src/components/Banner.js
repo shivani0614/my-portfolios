fbd658c9c1aa48ccfd8c5d0cb0c70c1533d23c24
shivani0614 <shivanialeti14@gmail.com>
2024-02-10 19:14:29 -0700

    updated the filefor alignments

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,54 +1,63 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header-img.svg";
-import { ArrowRightCircle } from 'react-bootstrap-icons';
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
-import meter1 from '../assets/img/meter1.svg';
-import shivani1 from '../assets/img/shivani1.jpeg';
-
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+import meter1 from "../assets/img/meter1.svg";
+import shivani1 from "../assets/img/shivani1.jpeg";
 
 export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Web Developer", "Web Designer", "Frontend Developer", "Full stack developer" ];
-  const period = 2000;
+  const toRotate = [
+    "Web Developer",
+    "Web Designer",
+    "Frontend Developer",
+    "Full stack developer",
+    "Application Developer"
+  ];
+  const period = 1000;
 
   useEffect(() => {
     let ticker = setInterval(() => {
       tick();
     }, delta);
 
-    return () => { clearInterval(ticker) };
-  }, [text])
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
 
   const tick = () => {
     let i = loopNum % toRotate.length;
     let fullText = toRotate[i];
-    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
 
     setText(updatedText);
 
     if (isDeleting) {
-      setDelta(prevDelta => prevDelta / 2);
+      setDelta((prevDelta) => prevDelta / 2);
     }
 
     if (!isDeleting && updatedText === fullText) {
       setIsDeleting(true);
-      setIndex(prevIndex => prevIndex - 1);
+      setIndex((prevIndex) => prevIndex - 1);
       setDelta(period);
-    } else if (isDeleting && updatedText === '') {
+    } else if (isDeleting && updatedText === "") {
       setIsDeleting(false);
       setLoopNum(loopNum + 1);
       setIndex(1);
       setDelta(500);
     } else {
-      setIndex(prevIndex => prevIndex + 1);
+      setIndex((prevIndex) => prevIndex + 1);
     }
-  }
+  };
 
   return (
     <section className="banner" id="home">
@@ -56,25 +65,61 @@ export const Banner = () => {
         <Row className="aligh-items-center">
           <Col xs={12} md={6} xl={7}>
             <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <span className="tagline">Welcome to My Portfolio</span>
-                <h1>{`Hi! I'm Shivani Reddy Aleti`} <span className="txt-rotate" dataPeriod="100" data-rotate='[ "Web Developer", "Web Designer", "Frontend Developer", "Full stack developer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>I have recently finished my Master's degree in Computer Science from the University of Texas at Arlington. I have previous experience as a software developer at DBS Bank and have a strong fascination for the arts. I thoroughly enjoy delving into novel skills and technologies. Dedicated front-end developer with 4 years of experience creating responsive, user-friendly web applications. Proficient in HTML5, CSS3, JavaScript, React JS, Angular JS, Databases, SQL. Adept at collaborating with cross-functional teams to deliver high-quality projects meeting both client and business requirements</p>
-                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
-              </div>}
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <span className="tagline">Welcome to My Portfolio</span>
+                  <h1>
+                    {`Hi! I'm Shivani Reddy Aleti`}{" "}
+                    
+                  </h1>
+                  <h1>
+                  <span
+                      className="txt-rotate"
+                      dataPeriod="100"
+                      data-rotate='[ "Web Developer", "Web Designer", "Frontend Developer", "Full stack developer" ]'
+                    >
+                      <span className="wrap">{text}</span>
+                    </span>
+                  </h1>
+                  <p>
+                    I have recently finished my Master's degree in Computer
+                    Science from the University of Texas at Arlington. I have
+                    previous experience as a software developer at DBS Bank and
+                    have a strong fascination for the arts. I thoroughly enjoy
+                    delving into novel skills and technologies. Dedicated
+                    front-end developer with 4 years of experience creating
+                    responsive, user-friendly web applications. Proficient in
+                    HTML5, CSS3, JavaScript, React JS, Angular JS, Databases,
+                    SQL. Adept at collaborating with cross-functional teams to
+                    deliver high-quality projects meeting both client and
+                    business requirements
+                  </p>
+                  <button onClick={() => console.log("connect")}>
+                    Let’s Connect <ArrowRightCircle size={25} />
+                  </button>
+                </div>
+              )}
             </TrackVisibility>
           </Col>
           <Col xs={12} md={6} xl={5}>
             <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
-                  <img className="shivani1"src={shivani1} alt="Header Img"/>
-                </div>}
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__zoomIn" : ""
+                  }
+                >
+                  <img className="shivani1" src={shivani1} alt="Header Img" />
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
     </section>
-  )
-}
+  );
+};
